19207ce19f467d21ad942c0870acdb7d116835b2
Yehia Elborma <borma425@gmail.com>
2024-10-21 16:10:02 +0300

    fix update cons pros keses

--- single.php
+++ single.php
@@ -11,7 +11,7 @@ $post_id    = get_the_ID();
   // Retrieve schema options
   $meta_key   = 'wp_review_schema_options';
   $meta_value = get_post_meta($post_id, $meta_key, true);
-  
+
   // Ensure $meta_value is an array
   if (!is_array($meta_value)) {
     // Attempt to decode if it's JSON
@@ -26,9 +26,36 @@ $post_id    = get_the_ID();
 }
 
 
-$product_name            = isset($meta_value['Product']['name']) ? $meta_value['Product']['name'] : '';
 
-$context['product_name'] = $product_name ;
+
+$context['product_name'] = isset($meta_value['Product']['name']) ? $meta_value['Product']['name'] : '';
+
+if (is_array($meta_value) && isset($meta_value['Product']['image'])) {
+    $image_data = $meta_value['Product']['image'];
+    $image_id = isset($image_data['id']) ? $image_data['id'] : '';
+    $image_url = isset($image_data['url']) ? $image_data['url'] : '';
+
+    // Check if image URL exists and output it
+    if (!empty($image_url)) {
+          $reviewed_item_image = $image_url; // Get existing image URL
+
+    } else {
+    $reviewed_item_image = "No_imagerM"; // Get existing image URL
+    $reviewed_item_image = get_post_meta($post_id, 'wp_review_hello_bar_bg_image_url', true);
+
+    }
+} else {
+    echo 'Product image data is not set correctly.';
+    $reviewed_item_image = "No_imagerTT"; // Get existing image URL
+}
+
+$context['product_image'] = $reviewed_item_image;
+$context['product_desc'] =  isset($meta_value['Product']['description']) ? $meta_value['Product']['description'] : '';
+$context['product_brand'] =  isset($meta_value['Product']['brand']) ? $meta_value['Product']['brand'] : '';
+$context['product_sku'] =   isset($meta_value['Product']['sku']) ? $meta_value['Product']['sku'] : '';
+$context['product_price'] =   isset($meta_value['Product']['price']) ? $meta_value['Product']['price'] : '';
+$context['product_total_rating'] =   get_post_meta( $post->ID, 'wp_review_total', true );
+$context['product_reviewBody'] =    get_post_meta($post->ID, 'wp_review_desc', true);
 
 
 
