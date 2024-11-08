520d92e8fa03e9e71337d92bbc28dd34a67a20d8
Yehia Elborma <borma425@gmail.com>
2024-11-08 21:04:57 +0200

    search page

--- archive.php
+++ archive.php
@@ -5,6 +5,32 @@ $context = Timber::context();
 
 $context['is_front_page'] = false;
 
-$context['brands'] = get_sorted_posts_by_taxonomy('post_tag', 'brands', -1);
 
-Timber::render('archive-brands.twig', $context);
+
+
+// Get the current category object
+$category = get_queried_object();
+
+// Check if the object is a category and then use it
+if (is_category()) {
+    // Get category ID
+    $category_id = $category->term_id;
+    // Get category name
+    $category_name = $category->name;
+    // Get category URL
+    $category_link = get_category_link($category_id);
+
+
+$context['cat_name'] = esc_html($category_name);
+
+$context['posts'] = Timber::get_posts(array(
+    'post_type' => 'post',
+    'posts_per_page' => 8, // Adjust the number of posts as needed
+    'cat' => $category_id, // Query posts by current category ID
+));
+
+Timber::render('archive.twig', $context);
+
+}
+
+
--- search.php
+++ search.php
@@ -3,65 +3,15 @@
 
 if (  get_query_var('s')  ) {
 
-
 $search    = strip_tags( (string) wp_unslash( get_query_var('s') ) );
 
 $args = array(
-    'post_type'      => array( 'roms','emulators'),
+    'post_type'      => array( 'post'),
     's' => $search,
-    'posts_per_page' => 10,
+    'posts_per_page' => 30,
     'order' => 'ASC',
     'paged' => $paged,
 );
-
-
-
-
-
-
-if ( isset($_GET['k']) && isset($_GET['v'])  ) {
-
-    // Get the current URL parameter tag Name
-    $key = $_GET['k'];
-    $key    = strip_tags( (string) wp_unslash( $key ) );
-    $value = $_GET['v'];
-    $value    = strip_tags( (string) wp_unslash( $value ) );
-
-
-    $array_acf = array(
-        'meta_query' => array(
-            array(
-                'key'     => $key,  // Meta key to filter by
-                'value'   => $value,       // Value to match
-                'compare' => 'LIKE',           // Comparison operator
-            ),
-        ),
-    );
-
-$args = array(
-        's' => "",
-    'post_type'      => array( 'roms','emulators'),
-        'posts_per_page' => 10,
-        'order' => 'ASC',
-$array_acf
-    );
-
-
-    $args = array_merge( $args, $array_acf );
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
 
 $context = Timber::context();
 $context["posts"] = Timber::get_posts( $args  );
