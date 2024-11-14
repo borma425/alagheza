8c0a4cc7acbbd7f4fd27584972e581775c73d148
borma <borma@localhost.localdomain>
2024-11-14 16:52:32 -0500

    tab swtich home page

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -9,7 +9,7 @@ if( !is_admin() ) {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', [], $ver, 'all' );
     wp_enqueue_script("parent-js", asset_url('main.js','/js/'), [], $ver, true );
 
-if( is_home() or is_search() ) {
+if( !is_singular("post") ) {
     wp_enqueue_script("home-js", asset_url('home.js','/js/'), [], $ver, true );
     wp_enqueue_style( 'home-style', asset_url('home.css','/css/'), [], $ver, 'all' );
 }
