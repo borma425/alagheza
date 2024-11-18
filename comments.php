37c9af116307fe6cbd0027f4785cd9a5437c9911
borma <borma@localhost.localdomain>
2024-11-18 12:37:47 -0500

    remove unused js css

--- comments.php
+++ comments.php
@@ -1,17 +1,43 @@
 						<form  method="POST" action="<?php echo esc_url(get_option('siteurl')); ?>/wp-comments-post.php"  id="commentform">
-                        <input type="hidden" name="action" value="k_comment">
-		<input type="hidden" name="post_id" value="143071">
-        <input type="hidden" id="url" name="url" required="" value="admin">
-                        
-                        <label for="review">المراجعة:</label>
-							<textarea   aria-labelledby="review-label" id="review" name="comment"  rows="5" required=""></textarea>
+						<div class="form-group">
+    <input type="hidden" name="action" value="k_comment">
+    <input type="hidden" name="post_id" value="143071">
+    <input type="hidden" id="url" name="url" required="" value="admin">
+  </div>
 
-							<label   for="name">الاسم:</label>
-							<input  aria-labelledby="name-label" type="text" name="author" id="author" required="">
+  <div class="form-group">
+    <label id="review-label" for="review">المراجعة:</label>
+    <textarea 
+      aria-labelledby="review-label" 
+      id="review" 
+      name="comment" 
+      rows="5" 
+      placeholder="أدخل مراجعتك هنا..." 
+      required="">
+    </textarea>
+  </div>
 
-							<label for="email">البريد الإلكتروني:</label>
-							<input   aria-labelledby="email-label"  type="email" id="email" name="email" required="">
+  <div class="form-group">
+    <label id="name-label" for="author">الاسم:</label>
+    <input 
+      aria-labelledby="name-label" 
+      type="text" 
+      name="author" 
+      id="author" 
+      placeholder="أدخل اسمك" 
+      required="">
+  </div>
 
+  <div class="form-group">
+    <label id="email-label" for="email">البريد الإلكتروني:</label>
+    <input 
+      aria-labelledby="email-label" 
+      type="email" 
+      id="email" 
+      name="email" 
+      placeholder="أدخل بريدك الإلكتروني" 
+      required="">
+  </div>
 							<button class="btn btn-primary" type="submit" name="submit" id="submit">
             <?php comment_id_fields(); ?>
 
