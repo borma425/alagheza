05d1ca9c767e6b702c50c9379e906dbe15506611
Yehia Elborma <42983220+borma425@users.noreply.github.com>
2025-01-12 18:45:54 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,12 +14,10 @@ require(get_theme_file_path('inc/enqueues.php'));
 require(get_theme_file_path('inc/shortcodes.php'));
 require(get_theme_file_path('inc/custom_post_type.php'));
 
-if( is_admin() ) {
     require(get_theme_file_path('inc/customizer/main.php'));
     require(get_theme_file_path('inc/acf/main.php'));
     require(get_theme_file_path('inc/admin.php'));
     require(get_theme_file_path('inc/ads_manager.php'));
     require(get_theme_file_path('inc/custom_code_manager.php'));
 
-}
 
